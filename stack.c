8a6ac84ae3bc9b664c9e8ec822bb7e0c22b5651a
DenisovGL <denisov0gleb@gmail.com>
2020-04-22 21:52:42 +0300

    Add pop and peek functions to stack [dgl]

--- stack.c
+++ stack.c
@@ -3,9 +3,6 @@
 #include <malloc.h>
 
 
-#define STACK_MAX 20
-
-
 typedef struct
 {
 	int top;
@@ -14,37 +11,22 @@ typedef struct
 } Stack;
 
 
-/*
- * int position = -1;
- */
+Stack* initStack(int max)
+{
+	Stack * st = (Stack*) malloc(sizeof(Stack));
+	st->data = (int*) malloc(sizeof(int) * max);
+	st->max = max;
+	st->top = -1;
+
+	return st;
+}
 
 
-/*
- * int push(int* Stack, int value, int max)
- * {
- *   if (position < max)
- *   {
- *     ++position;
- *     Stack[position] = value;
- *     puts("push OK!");
- *     return 0; // All is fine
- *   }
- *   else
- *   {
- *     puts("push bad!");
- *     return 1; // Error code
- *   }
- * }
- */
-
-int push(Stack * st, int value)
+int push(Stack* st, int value)
 {
 	if (st->max > st->top)
 	{
-		++st->top;
-		st->data[st->top] = value;
-
-		puts("push ok!");
+		st->data[++st->top] = value;
 		return 0;
 	}
 	else
@@ -55,28 +37,49 @@ int push(Stack * st, int value)
 }
 
 
+int pop(Stack* st)
+{
+	if (st->top >= 0)
+	{
+		return st->data[st->top--];
+	}
+	else
+		return -1;
+}
+
+
+int peek(Stack* st)
+{
+	return st->top >= 0 ? st->data[st->top] : -1;
+}
+
+
+void showStack(Stack* st)
+{
+	for (int i = st->top; i > 0; i--)
+	{
+		fprintf(stdout, "#%d = %d\n", i, st->data[i]);
+	}
+}
+
+
+void testStack(Stack* st1)
+{
+	for (int i = 0; i < 5; i++)
+		push(st1, st1->max - i);
+
+	showStack(st1);
+	fprintf(stdout, "The last one #%d = %d\n", st1->top+1, pop(st1));
+	showStack(st1);
+	fprintf(stdout, "Peek the last one #%d = %d\n", st1->top, peek(st1));
+	showStack(st1);
+}
+
+
 int main(void)
 {
-	Stack * st1 = (Stack*) malloc(sizeof(Stack));
-	st1->data = (int*) malloc(sizeof(int) * STACK_MAX);
-	st1->max = STACK_MAX;
-	st1->top = -1;
-	puts("Init ok!");
-
-	/*
-	 * int* Stack = (int*) malloc(sizeof(int) * STACK_MAX);
-	 * puts("Init ok!");
-	 */
-
-	for (int i = 0; i < 20; i++)
-	{
-		push(st1, STACK_MAX - i);
-	}
-
-	for (int i = 0; i < 20; i++)
-	{
-		fprintf(stdout, "#%d = %d\n", i, st1->data[i]);
-	}
+	Stack* st1 = initStack(20);
+	testStack(st1);
 
 	return 0;
 }
